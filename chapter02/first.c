61a89bd741bccef688c949b05e8f9fa75bf9a2b1
xuqinkun <xu_qinkun@163.com>
2026-07-08 16:51:23 +0800

    first c

---
+++ chapter02/first.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void test();
+
+int main(void)
+{
+    int num = 1;
+    int n, int n2, int n3;
+    printf("I am a student.\n");
+    test();
+    printf("First=%d\n", num);
+    getchar();
+    // return 0;
+}
+
+void test()
+{
+    printf("test\n");
+}
