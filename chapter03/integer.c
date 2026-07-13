b4801064d91d4c56c9176ad022d667576a7b04f4
xuqinkun <xu_qinkun@163.com>
2026-07-13 17:39:53 +0800

    feat: 更多整数类型

---
+++ chapter03/integer.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    long int estine;
+    long johns;
+
+    short int erns;
+    short jobs;
+
+    unsigned short a = -1;  // 65535
+
+    printf("a = %u\n", a);
+
+    printf("sizeof(int) = %lu, sizeof(short) = %lu, sizeof(long) = %lu\n", sizeof(int), sizeof(jobs), sizeof(estine));
+    return 0;
+}
