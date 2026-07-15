66ea3ac808a9334f6d38c5880e96edda96c98f7a
xuqinkun <xu_qinkun@163.com>
2026-07-15 14:32:38 +0800

    feat: 添加 chapter03 的 badcount.c 文件，展示错误的打印用法

---
+++ chapter03/badcount.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+int main()
+{
+    int m=4,n=5;
+    float f=7.0f, g=8.0f;
+    printf("%d\n", n, m);
+    printf("%d %d %d\n", n);
+    printf("%d %d\n", f, g);
+    return 0;
+}
