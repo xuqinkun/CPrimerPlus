e72ae7b7d4e993a5b7505ec729eddb3138f908b2
xuqinkun <xu_qinkun@163.com>
2026-07-15 11:18:50 +0800

    feat: 添加 chapter03 的 char.c 文件，包含字符输出示例

---
+++ chapter03/char.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 'ABCD';
+    // printf("a=%d\n", a);
+
+    char beep = '\007';
+    printf("\007");
+
+    printf("aaaa\vbbbb\vcccc");
+    getchar();
+    return 0;
+}
